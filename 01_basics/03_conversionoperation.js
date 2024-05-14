550e2a46ca32ec58936af2135475c4b82e989db1
Ranashazil <shazilr81@gmail.com>
2024-05-14 11:27:03 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,27 @@
+// let score = "33"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+// "33"=> 33
+// "33ssd" => Nan
+// true => 1 false => 0
+
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+// 0 => false 1 => true
+// "" => false "shazi" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
