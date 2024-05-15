50ad511071c934e6793632a619ed319a469db994
Ranashazil <shazilr81@gmail.com>
2024-05-15 06:54:37 +0000

    datatype Summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,39 @@
+// primitive (string, Number, Boolean, symbol, BigInt, null, undefined)
+// number
+const Number = 343
+let number = "12.3"
+// string
+ let name = "shazi"
+//  boolean
+const isLoggedIn = true
+// null
+const outsidetemp = null
+// undefined
+let useremail;
+//symbol
+let id = Symbol('123')
+const userId = Symbol('123')
+console.log(id === userId)
+// 
+let bigNumber = 42423431313n
+// non (reference) => (Arrays, object, functions)
+
+// Arrays
+
+let names = ["shazishiza", "Ammi", "family" ]
+
+// object
+
+let myObj = {
+    name: "shazishiza",
+    age: 23
+}
+
+// function
+
+const myFunction = function(){
+    console.log("hello world")
+}
+
+console.log( typeof userId)
+
